05814b999eb00c74c0ec42aff099c80ffce72921
2Stepan <stepan.cheprunov@mail.ru>
2026-03-24 19:04:10 +0700

    Сделал модель

--- Bid/Bidapp/models.py
+++ Bid/Bidapp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Request(models.Model):
+
+    STATUS_CHOICES = [
+        ("new", "Новая"),
+        ("in_progress", "В работе"),
+        ("done", "Завершена"),
+        ("canceled", "Отменена"),
+    ]
+
+    client_name = models.CharField(
+        max_length=255,
+        verbose_name="Имя клиента"
+    )
+
+    contacts = models.CharField(
+        max_length=255,
+        verbose_name="Контакты"
+    )
+
+    service = models.CharField(
+        max_length=255,
+        verbose_name="Услуга"
+    )
+
+    description = models.TextField(
+        blank=True,
+        verbose_name="Описание"
+    )
+
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="new",
+        verbose_name="Статус"
+    )
+
+    def __str__(self):
+        return f"Заявка {self.id} - {self.client_name}"
