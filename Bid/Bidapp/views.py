ea8746e0e2dd23a6b67f60a4f44c964f4252f516
AMENE1991 <alecseylusin9@gmail.com>
2026-03-26 01:25:53 +0700

    Добавил Get метод

--- Bid/Bidapp/views.py
+++ Bid/Bidapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Request
 # Create your views here.
 
@@ -38,3 +38,18 @@ def view_requests(request):
     requests_list = Request.objects.all()
     # Передаем список заявок в шаблон
     return render(request, 'requests/index.html', {'requests': requests_list})
+
+def get(request):
+    request_item = None
+    request_item_id = request.GET.get('request_item_id')
+
+    if request_item_id:
+        try:
+            request_item = Request.objects.get(id=request_item_id)
+        except Request.DoesNotExist:
+            request_item = None  # или любое другое действие
+
+    return render(request, 'request_get/index.html', {
+        'request_item': request_item,
+        'request_item_id': request_item_id,
+    })
